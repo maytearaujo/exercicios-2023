f19ec17a53331c1ea112cc57f091c78382f6fdeb
Maytê <maytearaujo@yahoo.com.br>
2023-10-17 16:10:21 -0300

    Tópicos

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -9,16 +9,30 @@ export class AppComponent {
   title = 'DevChuva';
   
 
- showTopico: boolean = true;
-
   show: boolean = false;
   showMessage(): void{
     this.show = !this.show;
-    this.showTopico = !this.showTopico ;
+  }
+
+  showCriarTopico: boolean = true;
+  showFormularioTopico: boolean = false;
+  showEnviarTopico: boolean = false;
+  showBtnCriarTopico: boolean = true;
+  showMessageTopico(): void{
+    this.showCriarTopico = !this.showCriarTopico;
+    this.showFormularioTopico = true;
   }
   
-  // showTopico: boolean = true;
-  // showMessageTopico(): void{
-  //   this.showTopico = !this.showTopico;
-  // }
+  showMessageEnviarTopico(): void{
+    this.showEnviarTopico = !this.showEnviarTopico;
+    this.showFormularioTopico = !this.showFormularioTopico;
+
+  }
+  
+  showMessageFormulario(): void{
+    this.showFormularioTopico = !this.showFormularioTopico;
+    this.showEnviarTopico = true;
+    this.showBtnCriarTopico = !this.showBtnCriarTopico;
+  }
+  
 }
